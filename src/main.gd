53d8ea351219723b47f307ee6fc64212419e7a08
Iceknith <dimi.ma@laposte.net>
2026-03-14 18:45:35 +0100

    added eof_meter

--- src/main.gd
+++ src/main.gd
@@ -8,14 +8,27 @@ enum GameState {
 	EndMenu,
 }
 
+signal eow_meter_changed(new_eow_var:float)
+
+@export var end_of_world_max_time_mins:float = 20
+@export var end_of_world_change_interval_s:float = 0.5
+@onready var eow_delta:float = end_of_world_change_interval_s / (end_of_world_max_time_mins * 60)
+var eow_meter:float:
+	set(new_val):
+		eow_meter = new_val
+
 @onready var world3D:Node3D = $World
 @onready var minigame_container:Control = $HUD/Minigames
 
 var currentState:GameState = GameState.Game3D
 
 func _ready() -> void:
-	MainCommunicator.signalMain.connect(recieve_signal)
+	connect_signals()
 	reset_state()
+	create_eow_timers()
+
+func connect_signals():
+	MainCommunicator.signalMain.connect(recieve_signal)
 
 func recieve_signal(type:String, data):
 	match type:
@@ -59,3 +72,30 @@ func show_minigame(minigameScene:PackedScene):
 
 func show_game3D():
 	reset_state()
+
+func create_eow_timers():
+	var timer_eow = Timer.new()
+	var timer_eow_update = Timer.new()
+	
+	timer_eow.timeout.connect(end_of_world)
+	timer_eow.one_shot = true
+	add_child(timer_eow)
+	timer_eow.start(end_of_world_max_time_mins * 60)
+	
+	connect_eow_update_timer(self, timer_eow_update.timeout)
+	add_child(timer_eow_update)
+	timer_eow_update.start(end_of_world_change_interval_s)
+
+func connect_eow_update_timer(node:Node, timer_timeout:Signal):
+	if not node: return
+	
+	for child in node.get_children():
+		if child.get("eow_meter") != null:
+			timer_timeout.connect(increment_eow_meter.bind(child))
+		connect_eow_update_timer(child, timer_timeout)
+
+func increment_eow_meter(node:Node):
+	node.eow_meter += eow_delta
+
+func end_of_world():
+	print("world ended")
