e34f638c7d2e3a7439c0923c7e8f024d4188d367
Iceknith <dimi.ma@laposte.net>
2026-03-24 23:55:20 +0100

    added skybox change

--- src/3D/world.gd
+++ src/3D/world.gd
@@ -8,10 +8,10 @@ var eow_meter:float = 0:
 @export var sunStartAngle:float = -10
 @export var sunEndAngle:float = -170
 
-@export var skyTopStartAngle:Color
-@export var skyBotStartAngle:Color
-@export var skyTopEndtAngle:Color
-@export var skyBotEndtAngle:Color
+@export var skyTopStartColor:Color
+@export var skyBotStartColor:Color
+@export var skyTopEndColor:Color
+@export var skyBotEndColor:Color
 @export var skyChangeCurve:Curve
 
 @onready var skyMaterial:ProceduralSkyMaterial = $WorldEnvironment.environment.sky.sky_material
@@ -19,3 +19,6 @@ var eow_meter:float = 0:
 func update_eow():
 	$Sun.rotation_degrees.x = sunStartAngle * (1 - eow_meter) + sunEndAngle * eow_meter
 	
+	var skyChangeProgress:float = skyChangeCurve.sample(eow_meter)
+	skyMaterial.sky_top_color = skyTopStartColor * (1 - skyChangeProgress) + skyTopEndColor * skyChangeProgress
+	skyMaterial.sky_horizon_color = skyBotStartColor * (1 - skyChangeProgress) + skyBotEndColor * skyChangeProgress
