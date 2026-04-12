a439fab1abf30213252e424883883a207720c01c
Iceknith <dimi.ma@laposte.net>
2026-04-13 01:03:12 +0200

    changed electician minigame texture

--- src/main.gd
+++ src/main.gd
@@ -41,7 +41,7 @@ func _ready() -> void:
 	create_eow_timers()
 	
 	# Debug
-	DebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
+	#wDebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
 
 func connect_signals():
 	MainCommunicator.signalMain.connect(receive_signal)
