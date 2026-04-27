f04cb111299074783c95c9ccdacbc52332a80e17
Ib3600 <Ibrahim2005elouzzani@gmail.com>
2026-04-28 00:48:00 +0200

    Je commit pour pas perdre ce que j'ai fait

--- src/minigames/arcadeGame/joe_manager.gd
+++ src/minigames/arcadeGame/joe_manager.gd
@@ -1,75 +1,84 @@
-class_name JoManager extends Node2D
+class_name JoManager
+extends Node2D
 
+@export var attack_cooldown: float = 3
+@export var enemy: ArcadeEnemy
+@onready var player: arcadePlayer = $"../Player"
+@onready var ball: arcade_ball = $"../../Ball"
+@export var distance_to_hit = 100
+@export var distance_max_to_ball = 75
+var pause_movement: bool
+var dir_to_ball: Vector2
+@export var JoCatchRate: Curve
+@export var max_manageable_speed: int = 500
+signal has_rolled_probabilities
+var rolled_probabilities: bool = false
 
-@export var attack_cooldown:float = 3
-
-@export var enemy:ArcadeEnemy
-@onready var player:arcadePlayer = $"../Player"
-@onready var ball:arcade_ball = $"../../Ball"
-
-@export var distance_to_hit = 200
-@export var distance_max_to_ball = 150
-
-var pause_movement:bool
-var dir_to_ball:Vector2
-
-
-enum opponent 
-{
+enum opponent {
 	LittleJo,
 	BigJo
 }
 
+func _ready():
+	has_rolled_probabilities.connect(reset_roll)
+
 func _process(delta):
-	if !pause_movement : 
+	if !pause_movement:
 		manage_jo_input(delta)
-	else : 
+	else:
 		enemy.velocity = Vector2.ZERO
-	
-	
-	
-func manage_jo_input(delta):
-	
-	dir_to_ball =  ball.global_position - enemy.global_position
-	if !player.HitBallComponent.launching_ball :  
-		movement_when_player_not_launching()
-	else : 
-		movement_when_player_launching()
-		
-	if dir_to_ball.length() < distance_to_hit : 
-		set_launch_direction()
-		enemy.HitBallComponent.hit_ball()
-		
-	
-	
-
-func set_pause_movement(v:bool):
-	pause_movement = v
-	
-func movement_when_player_not_launching(): #when player is not launching we try to get closer to the ball
-	var dir_to_move = dir_to_ball.normalized()
-	dir_to_move.x = sign(dir_to_move.x)
-	dir_to_move.y = sign(dir_to_move.y)
-	if (dir_to_ball.length() > distance_max_to_ball):
-		enemy.set_input_direction(dir_to_move)
-	else :
 		enemy.set_input_direction(Vector2.ZERO)
+
+func manage_jo_input(delta):
+	dir_to_ball = ball.global_position - enemy.global_position
 	
+	if !player.HitBallComponent.launching_ball:
+		movement_when_player_not_launching()
+	else:
+		movement_when_player_launching()
+	
+	if dir_to_ball.length() < distance_to_hit:
+		var launch_probability =0.0
+		
+		if !rolled_probabilities:
+			launch_probability = clamp(
+				JoCatchRate.sample(enemy.movementComponent.get_speed() / max_manageable_speed),
+				0.0, 1.0
+			)
+			rolled_probabilities = true
+			has_rolled_probabilities.emit()
+		
+		var succes_probability = randf()
+		
+		if succes_probability <= launch_probability:
+			set_launch_direction()
+			enemy.HitBallComponent.hit_ball()
 	
 
 
+func set_pause_movement(v: bool):
+	pause_movement = v
 
-func movement_when_player_launching(): #when player is launching we try to get away
-	var dir_to_move =  enemy.global_position - player.global_position
+func movement_when_player_not_launching():
+	var dir_to_move = dir_to_ball.normalized()
+	
+	if dir_to_ball.length() > distance_max_to_ball:
+		enemy.set_input_direction(dir_to_move)
+	else:
+		enemy.set_input_direction(Vector2.ZERO)
+
+func movement_when_player_launching():
+	var dir_to_move = enemy.global_position - player.global_position
 	var direction = dir_to_move.normalized()
+	
 	direction.x = sign(direction.x)
 	direction.y = sign(direction.y)
-	if (dir_to_ball.length() > distance_max_to_ball):
+	
+	if dir_to_ball.length() > distance_max_to_ball:
 		enemy.set_input_direction(direction)
-	else :
+	else:
 		enemy.set_input_direction(Vector2.ZERO)
-	
-	
+
 func set_launch_direction():
 	if randi() % 3 == 0:
 		var randomDirection = Vector2([-1, 1].pick_random(), [-1, 1].pick_random())
@@ -77,3 +86,7 @@ func set_launch_direction():
 	else:
 		var launchDirection = (player.global_position - enemy.global_position).normalized()
 		enemy.set_aiming_direction(launchDirection)
+
+func reset_roll():
+	await get_tree().create_timer(0.1).timeout
+	rolled_probabilities = false
