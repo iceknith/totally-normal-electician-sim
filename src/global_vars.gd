b01461f2d7bf27d1f41205f658d972a92d04c110
Iceknith <dimi.ma@laposte.net>
2026-05-14 13:54:04 +0200

    added boulangerie dialogue

--- src/global_vars.gd
+++ src/global_vars.gd
@@ -52,7 +52,6 @@ var visited_superette:bool = false
 var bought_cake_bakery:bool = false
 var visited_bakery:bool = false
 
-var has_cake:bool = false
 var has_decorations:bool = false
 
 ################
