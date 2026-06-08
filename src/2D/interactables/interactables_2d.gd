837072dedff474e1503d5978addbf7e59b5bda6f
Iceknith <dimi.ma@laposte.net>
2026-06-09 00:28:54 +0200

    placed newspaper, added musics, and miscs

--- src/2D/interactables/interactables_2d.gd
+++ src/2D/interactables/interactables_2d.gd
@@ -23,9 +23,9 @@ enum ActionType
 
 func _ready() -> void:
 	if interractable_dialogue == null: push_error("Interractable dialogue should be set !")
-	#interractable_label = find_label(interractable_dialogue)
-	#if interractable_label == null: push_error("Interractable dialogue should have a label !")
-	#interractable_label.text = GlobalVars.get_action_controls("interact1")
+	interractable_label = find_label(interractable_dialogue)
+	if interractable_label == null: push_error("Interractable dialogue should have a label !")
+	interractable_label.text = GlobalVars.get_action_controls("interact1")
 	
 	match action_type:
 		ActionType.Click:
