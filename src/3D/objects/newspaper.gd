439bd62600bed89c69cad30437885f1bbd1513cf
Iceknith <dimi.ma@laposte.net>
2026-06-08 20:47:19 +0200

    added base newspaper

---
+++ src/3D/objects/newspaper.gd
@@ -0,0 +1,32 @@
+class_name Newspaper extends Interactable3D
+
+@onready var sprite:Sprite3D = $Sprite3D
+
+static var newspaper_show_scene:PackedScene = preload("res://src/minigames/newspaper_show.tscn")
+static var newspaper_directory:String = "res://assets/misc/newspaper/"
+static var newspaper_textures:Array[Texture] = []
+
+static func init_textures():
+	newspaper_textures.clear()
+	for texture in ResourceLoader.list_directory(newspaper_directory):
+		newspaper_textures.append(load(newspaper_directory + texture))
+
+
+func _ready() -> void:
+	if newspaper_textures.is_empty(): init_textures()
+	else:
+		sprite.texture = newspaper_textures.pop_at(
+			randi_range(0, newspaper_textures.size()-1)
+			)
+	super()
+
+func start_interaction():
+	MainCommunicator.send_signal_to_main(
+		MainCommunicator.SignalType.ADD_MINIGAME, 
+		[
+			newspaper_show_scene,
+			{"texture": sprite.texture}
+		]
+		)
+	MainCommunicator.send_signal_to_main(MainCommunicator.SignalType.ELEM_DELETED, self)
+	queue_free.call_deferred()
