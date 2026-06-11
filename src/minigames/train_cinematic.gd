58b6bbcd63c360747f004d12ed32e595e81bc212
Ib3600 <Ibrahim2005elouzzani@gmail.com>
2026-06-11 17:31:12 +0200

    d

---
+++ src/minigames/train_cinematic.gd
@@ -0,0 +1,19 @@
+extends Minigame
+
+@export var eow_delay:float = 20
+@export var eow_playing_time:float = 38.8
+
+func _ready() -> void:
+	super()
+	get_tree().create_timer(eow_delay).timeout.connect(start_eow_music)
+
+func start_eow_music():
+	$EOW_Player.play()
+	get_tree().create_timer(eow_playing_time).timeout.connect(end_of_world)
+
+func end_of_world():
+	MainCommunicator.send_signal_to_main(
+		MainCommunicator.SignalType.TRIGGER_END_OF_WORLD,
+		null
+		)
+	SoundManager.stop_music.emit()
