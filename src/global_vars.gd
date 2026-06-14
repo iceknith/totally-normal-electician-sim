79f3604f456bdeea3392eee6e421b0fcbbd017b2
Ib3600 <Ibrahim2005elouzzani@gmail.com>
2026-06-14 05:45:25 +0200

    e
    e

--- src/global_vars.gd
+++ src/global_vars.gd
@@ -17,7 +17,7 @@ var electrician_minigame_current_stage:ElectricianMinigame.Stages = ElectricianM
 signal tower_completed_value_change
 signal all_tower_completed
 var completed_all_tower:bool = false
-var tower_completed:int = 14:
+var tower_completed:int = 0:
 	set(new_val):
 		tower_completed = new_val
 		tower_completed_value_change.emit()
